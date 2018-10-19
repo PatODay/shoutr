4a36f285a265ee5a7e6caa6c1e5d92c06de0b140
PatODay <poday22@gmail.com>
2018-10-19 12:26:40 -0400

    Added method for `show` to shouts_controller
    Added method for `show` to shouts_controller

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,4 +1,8 @@
 class ShoutsController < ApplicationController
+  def show
+    @shout = Shout.find(params[:id])
+  end
+
   def create
     shout = current_user.shouts.create(shout_params)
     redirect_to root_path, redirect_options_for(shout)
@@ -12,9 +16,9 @@ class ShoutsController < ApplicationController
 
   def redirect_options_for(shout)
     if shout.persisted?
-      { notice: "Shouted Successfully" }
+      { notice: 'Shouted Successfully' }
     else
-      { alert: "Could not shout" }
+      { alert: 'Could not shout' }
     end
   end
 end
